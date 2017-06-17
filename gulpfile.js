c921062228491366acd0c78104c3808dd8ac1971
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-06-17 16:44:16 +0300

    tested at the terminal if gulpfile is working correctly and its a success

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+// Dependencies used in gulpfile.js. It entails both for the bower and modules
+
+var gulp = require('gulp'); /*Dependency used for displaying in the terminal*/
+
+/*Task used for displaying in the console*/
+gulp.task('myTask', function() {
+  console.log('hello gulp');
+});
