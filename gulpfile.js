0447d7a362f54e38953c0a6fb57041d604c1a4e3
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-06-17 16:46:38 +0300

    Added dependency for browerserification, for building app.js and concatination and its tasks and are all working

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
 // Dependencies used in gulpfile.js. It entails both for the bower and modules
 
 var gulp = require('gulp'); /*Dependency used for displaying in the terminal*/
+var browserify = require('browserify'); /*Dependency used for browserification*/
+var source = require('vinyl-source-stream'); /*Dependency used after building app.js*/
+var concat = require('gulp-concat'); /*Dependency used for concatination*/
 
 /*Task used for displaying in the console*/
 gulp.task('myTask', function() {
   console.log('hello gulp');
+});
+
+/*Task used for concatination*/
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+/*Task used for browserifying concatinated items*/
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({
+      entries: ['./tmp/allConcat.js']
+    })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
 });
